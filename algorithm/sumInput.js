47e30ed58f49e8e5d59f8a718e6d3a28a6bc15f6
공채원 <chaewonkong@gmail.com>
2018-07-19 00:22:59 -0700

    Node.js algorithm practice

--- algorithm/sumInput.js
+++ algorithm/sumInput.js
@@ -1,13 +1,2 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
-
-rl.question("", function(answer){
-	let arr = answer.split(" ");
-	const a = Number(arr[0]);
-	const b = Number(arr[1]);
-	console.log(a+b);
-	rl.close();
-});
+var a = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+console.log(parseInt(a[0])+parseInt(a[1]));
