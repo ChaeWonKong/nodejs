1f49eb37cf3c017729d61027840302561cc53fb1
공채원 <chaewonkong@gmail.com>
2018-07-29 13:04:07 -0700

    Update files

--- algorithm/sumInput.js
+++ algorithm/sumInput.js
@@ -1,2 +1,10 @@
-var a = require('fs').readFileSync('/dev/stdin').toString().split(' ');
-console.log(parseInt(a[0])+parseInt(a[1]));
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question('', function(answer){
+    let result = answer.split(' ');
+    console.log(Number(result[0]) + Number(result[1]));
+    rl.close();
+});
